70bda0543507c9cf36acaee26a22bf42d544a9bd
Rizki Yanuar <ryanuar83@gmail.com>
2023-10-25 07:54:56 +0700

    Revert "File OpenClose Tugas 4 StudentCinema"
    This reverts commit 1bcca2a524de85b76f0ffe884a6ae616d9cf4845.

--- openclose/StudentCinema.java
+++
@@ -1,9 +0,0 @@
-class StudentCinema extends Cinema {
-        public StudentCinema(Double price) {
-        this.price = price;
-    }
-    @Override
-    Double calculateAdminFee() {
-        return price * 2 / 100;
-    }
-}
